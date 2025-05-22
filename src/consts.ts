df8e2bcf4f3f87f3d28605827e8f72c9bd571649
Schwayro2 <christianchavez@lyhs.org>
2025-05-22 09:24:35 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Sapphire.lol";
+export const SITE_DESCRIPTION = "The Ultimate Service/Cheat Provider Trusted by Many – Delivering Unbeatable Quality, Reliable Products, and 24/7 Dedicated Support to Keep You Ahead in Every Game!";
